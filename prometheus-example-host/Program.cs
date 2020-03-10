598e56ee01bc939916b754d81ec52fe43f710f3d
Stephen Wike <stephen.wike@terumobct.com>
2020-03-10 13:16:07 -0600

    pushing latest

---
+++ prometheus-example-host/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace prometheus_example_host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
